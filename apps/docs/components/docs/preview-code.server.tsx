26365f129f5411d8e6a106911f1dbf4034926683
Ephraim Duncan <55143799+ephraimduncan@users.noreply.github.com>
2026-08-06 11:58:05 +0000

    fix(docs): show registry install path in PreviewCode imports (#5641)

--- apps/docs/components/docs/preview-code.server.tsx
+++ apps/docs/components/docs/preview-code.server.tsx
@@ -195,9 +195,7 @@ function cleanupImports(imports: string[]): string[] {
   return imports
     .filter((imp) => !imp.includes("SampleFrame"))
     .map((imp) =>
-      imp
-        .replace(/@\/components\/assistant-ui\//g, "@/components/ui/")
-        .replace(/(@\/components\/[\w-]+\/[\w.-]+?)\.radix(["'])/g, "$1$2"),
+      imp.replace(/(@\/components\/[\w-]+\/[\w.-]+?)\.radix(["'])/g, "$1$2"),
     );
 }
 
